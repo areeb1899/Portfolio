4d9b886e18fd7cf626d2e641ce0164366e22b4f8
Areeb Ahmed <ahmedareeb1819@gmail.com>
2024-04-26 00:44:34 +0530

    Portfolio v7

--- db/database.js
+++ db/database.js
@@ -4,8 +4,6 @@ const DB = process.env.DATABASE;
 
 
 mongoose.connect(DB, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
 }).then(() => console.log("DATABASE CONNECTED")).catch((err) => {
     console.log(err)
 })
